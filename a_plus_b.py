2ca70d62b850e0ac207f83acce8076079e4c5d06
GWANWOO KIM <gwk1125@hanyang.ac.kr>
2025-10-10 11:56:08 +0900

    Updated a+b

--- a_plus_b.py
+++ a_plus_b.py
@@ -1,3 +1,9 @@
-x = int(input())
-y = int(input())
-print(f'{x} + {y} = {x + y}')
+def a_plus_b(a):
+    result = a[0] + a[1]
+    return result
+
+
+l = [9, 13]
+z = a_plus_b(l)
+
+print(f'{l[0]} + {l[1]} = {z}')
